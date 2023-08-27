3dd037fc521580d897b2d6bef7546c63295526d1
DiegoHrz <hernandezwdiego@gmail.com>
2023-08-27 16:06:25 -0500

    Add 2 React-Estado-Lifecycle.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,37 @@
+
 export default function Card(props) {
+   const {name, species, gender, image, onClose, id} = props;
    return (
-      <div>
-         <button onClick={props.onClose}>X</button>
-         <h2>{props.name}</h2>
-         <h2>{props.status}</h2>
-         <h2>{props.species}</h2>
-         <h2>{props.gender}</h2>
-         <h2>{props.origin}</h2>
-         <img src={props.image} alt='name' />
-      </div>
+     <div>
+       <button
+         onClick={() => {
+           onClose(id);
+         }}
+       >
+         X
+       </button>
+       <h2>Name: {name}</h2>
+       {/* <h2>Status:{status}</h2> */}
+       <h2>Species: {species}</h2>
+       {/* <h2>Origin:{origin}</h2> */}
+       <h2>Gender: {gender}</h2>
+       <img src={image} alt={name} />
+     </div>
+   );
+ }
+ 
+
+// export default function Card(props) {
+//    return (
+//       <div>
+//          <button onClick={props.onClose}>X</button>
+//          <h2>{props.name}</h2>
+//          <h2>{props.status}</h2>
+//          <h2>{props.species}</h2>
+//          <h2>{props.gender}</h2>
+//          <h2>{props.origin}</h2>
+//          <img src={props.image} alt='name' />
+//       </div>
 
 // const {name, species, gender, image, onClose} = props;
 // return (
@@ -21,5 +44,5 @@ export default function Card(props) {
 //     <h2>Gender:{gender}</h2>
 //     <img src={image} alt={name} />
 //   </div>
-   );
-}
+//    );
+// }
