e35b688ff36260e75da9f969cb1b8441e74ec831
DiegoHrz <hernandezwdiego@gmail.com>
2023-08-31 23:45:43 -0500

    Add React redux

---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import{createStore, apply Middleware} from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducer';
+import { applyMiddleware } from 'redux';
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
