8e27831e53f23c7324f21bcb35979750d71b12f9
DiegoHrz <hernandezwdiego@gmail.com>
2023-09-20 18:46:39 -0500

    Add client

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from "react-redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducer";
-import { applyMiddleware } from "redux";
-
-export const store = createStore(rootReducer, applyMiddleware(thunk));
