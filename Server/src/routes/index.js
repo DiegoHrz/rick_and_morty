60c1c0da216a0a4a7588d2a9d22b1ab896e9d970
DiegoHrz <hernandezwdiego@gmail.com>
2023-10-01 20:56:43 -0500

    Add Homework 5 progresss

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -2,9 +2,8 @@ const getCharById = require("../controllers/getCharById");
 const { postFav, deleteFav } = require("../controllers/handleFavorites");
 const login = require("../controllers/login");
 
-const router = require('express')
+const router = require("express");
 
-router.get('/character/:id',(req,res)=>{
-    res.send()
-
-})
+router.get("/character/:id", (req, res) => {
+  res.send();
+});
